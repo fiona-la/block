06ae940e9005fba5f6efc49b3d647c56337cb3db
Jeff Zhang <yzha2577@uwo.ca>
2020-12-31 20:50:39 -0500

    Update Client to use quote of the day from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
-
+import { useEffect, useState } from 'react'
 function App() {
-    let quote = "A Quote Here"
+    useEffect(() => {
+        fetch("http://localhost:3001/qotd")
+        .then(res=>res.text())
+        .then(quote=>{
+            setQuote(quote);
+        })
+        .catch(err=>console.log(err))
+    }, [])
+    const [quote, setQuote] = useState("");
     let image = window.location.origin + "/monday.png";
     let now = new Date();
     return (
